6d0c03ce5913c8f2214ed2f2583ccf6b7c242875
dev-madilyn <lollin0@naver.com>
2023-04-28 12:33:39 +0900

    Chore: AngryCar, Roulette

---
+++ Roulette/Assets/Scripts/RouletteController.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RouletteController : MonoBehaviour
+{
+    float rotSpeed = 0; // 회전 속도
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // 클릭하면 회전 속도 설정
+        if (Input.GetMouseButtonDown(0))
+        {
+            this.rotSpeed = 10;
+        }
+
+        // 회전 속도만큼 룰렛 회전
+        transform.Rotate(0, 0, this.rotSpeed);
+
+        // 룰렛 회전 속도를 감속시킴
+        this.rotSpeed *= 0.98f;
+    }
+}
